4408ad5db94a6f3c39a3d64731f09b133f9c0eee
William.Tsai <william.tsai@kdanmobile.com>
2020-08-22 16:57:39 +0800

    Add required

--- app/controllers/api/book_controller.rb
+++ app/controllers/api/book_controller.rb
@@ -1,6 +1,7 @@
 class Api::BookController < ApplicationController
 
   def price
+    params.require([:price_from, :price_to])
     result = Book.search_price(params[:price_from], params[:price_to], params[:sort_by_price])
 
     api_response(200, 'success', result)
--- app/controllers/api/book_store_controller.rb
+++ app/controllers/api/book_store_controller.rb
@@ -10,24 +10,28 @@ class Api::BookStoreController < ApplicationController
   end
 
   def openhours
+    params.require([:hours, :more_or_less, :timespan])
     result = Store.seach_openhours(params[:hours], params[:more_or_less], params[:timespan])
 
     api_response(200, 'success', result)
   end
 
   def amount
+    params.require([:books_amount])
     result = Store.search_amounts(params[:books_amount], params[:more_or_less], params[:price_min], params[:price_max])
     
     api_response(200, 'success', result)
   end
 
   def name
+    params.require([:name])
     result = Store.search_name(params[:name])
 
     api_response(200, 'success', result)
   end
 
   def transaction_volume
+    params.require([:rank_type])
     result = Store.search_trans_volume(params[:rank_type])
 
     api_response(200, 'success', result)
